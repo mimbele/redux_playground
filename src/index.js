1270f2af419aa187465997d7bdbfaa0d7c0a6d78
mimbele <mrym.ndr@gmail.com>
2020-10-24 14:37:54 +0330

    adding a logger middleWare to the redux store

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
-import { createStore, combineReducers } from 'redux'
+import { createStore, combineReducers, applyMiddleware } from 'redux'
 import { Provider } from 'react-redux'
 import counterReducer from './store/reducers/counter'
 import resultsReducer from './store/reducers/results'
@@ -13,7 +13,18 @@ const reducers = combineReducers({
     rst: resultsReducer
 })
 
-const store = createStore(reducers)
+const logger = store => {
+    return next => {
+        return action => {
+            console.log('[MiddleWare] dispatching ', action)
+            const result = next(action)
+            console.log('[MiddleWare] nextState is ', store.getState())
+            return result
+        }
+    }
+}
+
+const store = createStore(reducers, applyMiddleware(logger))
 
 ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
 registerServiceWorker();
